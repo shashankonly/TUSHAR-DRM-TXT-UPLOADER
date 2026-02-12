cab1a7326fb5fafb529a4b4ff3db8be661d2805b
shashankonly <jaishreeram1671@gmail.com>
2026-02-12 18:43:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -352,7 +352,7 @@ async def upload(bot: Client, m: Message):
                 keys_string = " ".join([f"--key {key}" for key in keys])
               
             elif "d1d34p8vz63oiq" in url or "sec1.pw.live" in url:
-             url = f"https://anonymouspwplayerr-c96de7802811.herokuapp.com/pw?url={url}&token={raw_text4}"
+             url = f"https://anonymouspwplayerrr-3dba7e3fb6a8.herokuapp.com/pw?url={url}&token={raw_text4}"
              #url = f"https://anonymousrajputplayer-9ab2f2730a02.herokuapp.com/pw?url={url}&token={raw_text4}"
               
             elif "acecwply" in url:
